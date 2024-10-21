98bdcdf8453967c9a3de450d3ffc16bf1f60ebd4
0183f <bisheshta456@gmail.com>
2024-10-21 07:59:10 +0545

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
+// Register user
+router.post('/register', async (req, res) => {
+    // Code to register user
+});
+
+// Login user
+router.post('/login', async (req, res) => {
+    // Code to authenticate user
+});
+
+module.exports = router;
