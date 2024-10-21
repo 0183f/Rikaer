c94d3749a96c213646e65f7b4389e9beceda5fd5
0183f <bisheshta456@gmail.com>
2024-10-21 07:58:13 +0545

    Update gameSocket.js

--- sockets/gameSocket.js
+++ sockets/gameSocket.js
@@ -1,1 +1,11 @@
+module.exports = (socket, io) => {
+    socket.on('joinRoom', ({ roomCode, password }) => {
+        // Code to handle joining a room
+        socket.join(roomCode);
+        io.to(roomCode).emit('gameStart', { roomCode });
+    });
 
+    socket.on('searchStranger', () => {
+        // Code to find a random stranger for a game
+    });
+};
