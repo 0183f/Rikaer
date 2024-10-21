47a0f3b8ba517a74e156269ac24fbe9c327025e1
0183f <bisheshta456@gmail.com>
2024-10-21 07:59:56 +0545

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -1,1 +1,15 @@
+const socket = io();
 
+// Example for joining a game room
+function joinRoom(roomCode, password) {
+    socket.emit('joinRoom', { roomCode, password });
+}
+
+socket.on('gameStart', (gameData) => {
+    console.log('Game Started:', gameData);
+});
+
+// To search for a random stranger
+function searchStranger() {
+    socket.emit('searchStranger');
+}
